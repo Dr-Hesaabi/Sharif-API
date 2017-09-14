33fecf27d03067e0b3f5ada7aaa19e6193e7e603
rezaKhaleghiFar <khalghi75@gmail.com>
2017-09-14 12:10:03 -0700

    Updated a function in getPhone

--- AndycabarApi/Controllers/getPhoneController.cs
+++ AndycabarApi/Controllers/getPhoneController.cs
@@ -22,34 +22,65 @@ namespace AndycabarApi.Controllers
             Models.User tb = new Models.User();
             tb.Mobile = phone.phone;
             tb.VerificationCode = new Random().Next(1000, 9999);
-            db.Users.Add(tb);
+            
             var data = db.Users.Where(x => x.Mobile == phone.phone).ToList();
             if (data.Count()>0)
             {
-                var user =db.Users.Where(x=>x.)
-            }
-            db.SaveChanges();
-            var httpWebRequest = (HttpWebRequest)WebRequest.Create(" http://api.smsapp.ir/v2/sms/send/simple");
-            httpWebRequest.ContentType = "application/json";
-            httpWebRequest.Method = "POST";
-            httpWebRequest.Headers.Add("apikey", "HHweSCQ4jCniM2KYKNSTOgmTepEkjXiJOQP4uIcLNi4");
+                var user = db.Users.Where(x => x.Mobile == phone.phone).FirstOrDefault();
+                user.VerificationCode = tb.VerificationCode;
+                db.SaveChanges();
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create(" http://api.smsapp.ir/v2/sms/send/simple");
+                httpWebRequest.ContentType = "application/json";
+                httpWebRequest.Method = "POST";
+                httpWebRequest.Headers.Add("apikey", "HHweSCQ4jCniM2KYKNSTOgmTepEkjXiJOQP4uIcLNi4");
 
-            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+                using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+                {
+                    string json = "{\"receptor\":\"" + phone.phone + "\"," +
+                                  "\"message\":\"" + "اندیکابار \n کد تایید شماره شما : \n" + tb.VerificationCode + "\"," +
+                                 "\"sender\":\"30006703451451\"}";
+
+                    streamWriter.Write(json);
+                    streamWriter.Flush();
+                    streamWriter.Close();
+                }
+
+                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    var result = streamReader.ReadToEnd();
+                }
+                return tb.VerificationCode.ToString();
+
+            }
+            else
             {
-                string json = "{\"receptor\":\"" + phone.phone + "\"," +
-                              "\"message\":\"" + "اندیکابار \n کد تایید شماره شما : \n" + tb.VerificationCode + "\"," +
-                             "\"sender\":\"30006703451451\"}";
+                db.Users.Add(tb);
+                db.SaveChanges();
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create(" http://api.smsapp.ir/v2/sms/send/simple");
+                httpWebRequest.ContentType = "application/json";
+                httpWebRequest.Method = "POST";
+                httpWebRequest.Headers.Add("apikey", "HHweSCQ4jCniM2KYKNSTOgmTepEkjXiJOQP4uIcLNi4");
 
-                streamWriter.Write(json);
-                streamWriter.Flush();
-                streamWriter.Close();
-            }
+                using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+                {
+                    string json = "{\"receptor\":\"" + phone.phone + "\"," +
+                                  "\"message\":\"" + "اندیکابار \n کد تایید شماره شما : \n" + tb.VerificationCode + "\"," +
+                                 "\"sender\":\"30006703451451\"}";
 
-            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
-            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
-            {
-                var result = streamReader.ReadToEnd();
+                    streamWriter.Write(json);
+                    streamWriter.Flush();
+                    streamWriter.Close();
+                }
+
+                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    var result = streamReader.ReadToEnd();
+                }
             }
+            
+            
             return tb.VerificationCode.ToString();
 
         }
